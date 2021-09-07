11277723b5b749061a987cd7c231b9de21b1bdd4
aliceFrontend <aliceFrontend@users.noreply.github.com>
2021-09-07 13:37:48 +0300

    create new file responsive and file change main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,57 @@
+
+// Нажатие на кнопку "заказать"
 let order = document.querySelector('.auth__order-btn');
+
 order.onclick = function(){
     order.classList.toggle('disabled');
 }
-console.log();
+
+// Поиск
+let input = document.querySelector('.dictionary__search input');
+// Реакция на введение символов
+input.oninput = function (){
+    if(input.value.length > 0){
+        input.classList.add('dictionary__search--active');
+        input.setAttribute('placeholder', '');
+    } else {
+        input.classList.remove('dictionary__search--active');
+        input.setAttribute('placeholder', 'Начните писать...');
+    }
+}
+
+//Реакция на нажатие input
+input.onfocus = function (){
+    input.classList.add('dictionary__search--active');
+    input.setAttribute('placeholder', '');
+}
+//Реакция после нажатия на input
+input.onblur = function (){
+    input.classList.remove('dictionary__search--active');
+    input.setAttribute('placeholder', 'Начните писать...');
+}
+
+//коллекция кнопок dictionary__list-item
+let liItems = document.querySelectorAll('.dictionary__list-item');
+let liTmp = liItems[0];
+// активное сосотояние у плит и возврат значения в input
+for(let liItem of liItems){
+    liItem.onclick = function(){
+        liItem.classList.add('active');
+        if(liTmp.classList.contains('active') && liItem.classList.contains('active')){
+            liTmp.classList.remove('active');
+        } else{
+        liItem.classList.add('active');
+        }
+        liTmp = liItem;
+        // возвращаем значение li в input
+        input.value = liTmp.textContent;
+        // активное состояние input
+        input.oninput();
+    }
+}
+//функция исключения для первого элемента
+liItems[0].onclick = function () {
+    liItems[0].classList.add('active');
+    input.value = liItems[0].textContent;
+    // input.oninput();
+}
