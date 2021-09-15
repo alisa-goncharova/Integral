ec4ccae7840faa8e052bc18a28e0b02be4cc4351
aliceFrontend <aliceFrontend@users.noreply.github.com>
2021-09-15 21:36:28 +0300

    update
    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
-
+let body = document.querySelector('body');
 // Нажатие на кнопку "заказать"
 let order = document.querySelector('.auth__order-btn');
 
 order.onclick = function(){
     order.classList.toggle('disabled');
 }
-
 //нажатие на пункт списка
 let navBar = document.querySelector('.header__navs');
 let navLinks = document.querySelectorAll('.header__navs a');
@@ -19,28 +18,41 @@ navBar.addEventListener('click', function(event) {
     event.target.classList.add('header__nav-item--active');
 });
 // конец
+
 //нажатие на иконку аккаунта
 let signBtn = document.querySelector('.auth__sign-btn');
-
+let modalAuth = document.querySelector('.auth__modal');
 signBtn.onclick = function(){
     signBtn.classList.toggle('auth__order-btn--active');
-}
-
-// Поиск
-let input = document.querySelector('.dictionary__search input');
-// Реакция на введение символов
-input.oninput = function (){
-    if(input.value.length > 0){
-        input.classList.add('dictionary__search--active');
-        input.setAttribute('placeholder', '');
-    } else {
-        input.classList.remove('dictionary__search--active');
-        input.setAttribute('placeholder', 'Начните писать...');
-        //удаление активной плитки
-        liTmp.classList.remove('active');
+    modalAuth.classList.toggle('auth__modal--show');
+    let width = screen.width;
+    if(width <= 500){
+        document.querySelector('.header__logo').classList.toggle('none');
+        document.querySelector('.auth__order-btn').classList.toggle('none');
+        document.querySelector('.header__mobile-burger').classList.toggle('mobile-burger--active');
+        document.querySelector('.header').classList.toggle('header__sign--show');
+        // body.classList.toggle('hidden');
+        if(document.querySelector('.auth__modal').classList.contains('auth__modal--show')){
+            body.classList.add('hidden');
+        } else{
+            body.classList.remove('hidden');
+        }
     }
 }
-
+// // Поиск
+let input = document.querySelector('.dictionary__search input');
+// // Реакция на введение символов
+// input.oninput = function (){
+//     if(input.value.length > 0){
+//         input.classList.add('dictionary__search--active');
+//         input.setAttribute('placeholder', '');
+//     } else {
+//         input.classList.remove('dictionary__search--active');
+//         input.setAttribute('placeholder', 'Начните писать...');
+//         //удаление активной плитки
+//         liTmp.classList.remove('active');
+//     }
+// }
 //Реакция на нажатие input
 input.onfocus = function (){
     input.classList.add('dictionary__search--active');
@@ -51,35 +63,80 @@ input.onblur = function (){
     input.classList.remove('dictionary__search--active');
     input.setAttribute('placeholder', 'Начните писать...');
 }
-
 //коллекция кнопок dictionary__list-item
-let liItems = document.querySelectorAll('.dictionary__list-item');
-let liTmp = liItems[0];
-// активное сосотояние у плит и возврат значения в input
-for(let liItem of liItems){
-    liItem.onclick = function(){
-        liItem.classList.add('active');
-        if(liTmp.classList.contains('active') && liItem.classList.contains('active')){
-            liTmp.classList.remove('active');
-        } else{
-        liItem.classList.add('active');
-        }
-        liTmp = liItem;
-        // возвращаем значение li в input
-        input.value = liTmp.textContent;
-        // активное состояние input
-        input.oninput();
-    }
-}
-//функция исключения для первого элемента
-// liItems[0].onclick = function () {
-//     liItems[0].classList.add('active');
-//     input.value = liItems[0].textContent;
-//     input.oninput();
-// }
+let liList = document.querySelector('.dictionary__content');
+let liItems = document.querySelectorAll('.dictionary__content li');
+liList.addEventListener('click', function(event) {
+    event.preventDefault(); // остановим работу ссылки, нам же надо поменять класс а не уйти со страницы
+    // Пробежимся по плиткам и удалим active класс, если вдруг он где-то есть
+    liItems.forEach(function(element, index) {
+        element.classList.remove('active');
+    });
+    // Переключим для элемента на который кликнули класс
+    event.target.classList.add('active');
+});
+
 //адаптивное меню
 let burger = document.querySelector('.header__mobile-burger');
 
 burger.onclick = function(){
-    burger.classList.toggle('change');
+    burger.classList.toggle('mobile-burger--active');
+    // проверка на наличие открытого модального окна
+    if(document.querySelector('.header__mobile-modal').classList.contains('header__mobile-modal--show')){
+        body.classList.remove('hidden');
+    } else{
+        body.classList.add('hidden');
+    }
+    if(document.querySelector('.auth__modal').classList.contains('auth__modal--show')){
+        burger.classList.toggle('mobile-burger--active');
+        signBtn.onclick();
+    } else{
+    document.querySelector('.header__mobile-modal').classList.toggle('header__mobile-modal--show');
+        let width = screen.width;
+        if(width <= 500){
+            document.querySelector('.auth__order-btn').classList.toggle('none');
+            document.querySelector('.auth__sign-btn').classList.toggle('none');
+        }
+    }
 }
+
+// темная  тема
+let inputTheme = document.querySelector('.modal__switch input');
+
+inputTheme.onclick = function (){
+    if(inputTheme.checked){
+        console.log('light');
+        document.querySelector('.modal__dark').classList.remove('modal__dark--active');
+        document.querySelector('.modal__light').classList.add('modal__light--active');
+    } else{
+        console.log('dark');
+        document.querySelector('.modal__dark').classList.add('modal__dark--active');
+        document.querySelector('.modal__light').classList.remove('modal__light--active');
+    }
+}
+
+// нажатие на пункты модального меню при клике на акк
+let modalList = document.querySelector('.auth__modal-list');
+let modalListItem = document.querySelectorAll('.auth__modal-list a');
+modalList.addEventListener('click', function(event) {
+    event.preventDefault(); // остановим работу ссылки, нам же надо поменять класс а не уйти со страницы
+    // Пробежимся по ссылкам и удалим active класс, если вдруг он где-то есть
+    modalListItem.forEach(function(element, index) {
+        element.classList.remove('auth__modal-item--active');
+    });
+    // Переключим для элемента на который кликнули класс
+    event.target.classList.add('auth__modal-item--active');
+});
+
+//нажатие на мобильное меню
+let mobileNavs = document.querySelector('.header__mobile-navs');
+let mobileNavsItem = document.querySelectorAll('.header__mobile-navs a');
+mobileNavs.addEventListener('click', function(event) {
+    event.preventDefault(); // остановим работу ссылки, нам же надо поменять класс а не уйти со страницы
+    // Пробежимся по ссылкам и удалим active класс, если вдруг он где-то есть
+    mobileNavsItem.forEach(function(element, index) {
+        element.classList.remove('header__nav-item--active');
+    });
+    // Переключим для элемента на который кликнули класс
+    event.target.classList.add('header__nav-item--active');
+});
